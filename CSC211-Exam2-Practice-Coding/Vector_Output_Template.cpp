9d4bb3eb15ad6f2b693554b0428b55a10535638a
yapalfredo <36459847+yapalfredo@users.noreply.github.com>
2018-11-29 09:11:10 -0500

    exam2 practice 11/29/2018

---
+++ CSC211-Exam2-Practice-Coding/Vector_Output_Template.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+template <typename T>
+
+void squareIterator(vector<T> vec, int SIZE)
+{
+	if (SIZE <= 0) throw string("Invalid Vector Size!");
+
+	ofstream outputFile("output.txt", ios::out);
+}
+
+
+int main()
+{
+
+	try
+	{
+		
+	}
+	catch (string str)
+	{
+		cout << str << endl;
+	}
+
+	return 0;
+}
